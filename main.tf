a11a4f8b691c508521474062896641c24aba8550
Bharat Bhushan <111484672+bharatbhushan05@users.noreply.github.com>
2025-11-13 12:33:59 +0530

    nodes-deployed

--- main.tf
+++ main.tf
@@ -1,8 +1,17 @@
-resource "aws_instance" "my_ec2" {
+resource "aws_instance" "master_ec2" {
   ami           = "ami-0ecb62995f68bb549" # Ubuntu 22.04 (update as per region)
   instance_type = var.instance_type
 
   tags = {
-    Name = "GitHubActions-EC2"
+    Name = "master-node"
   }
 }
+
+resource "aws_instance" "worker_ec2" {
+  ami           = "ami-0ecb62995f68bb549" # Ubuntu 22.04 (update as per region)
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "worker-node"
+  }
+}`
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "ec2_public_ip" {
-  value = aws_instance.my_ec2.public_ip
+  value = aws_instance.master_ec2.public_ip
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.worker_ec2.public_ip
 }
